991f6a52777e43ce83f7561a015eea3fcfce19d9
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-24 15:12:52 -0300

    Create dobro_de_um_numero.cpp

---
+++ dobro_de_um_numero.cpp
@@ -0,0 +1,21 @@
+ #include <iostream>
+using namespace std;
+
+int dobro(int a, int b){
+    return a * b;
+}
+
+int main(){
+    int a, b;
+    
+    cout << "Digite o primeiro número: ";
+    cin >> a;
+    
+    cout << "Digite o segundo número: ";
+    cin >> b;
+    
+    int resultado = dobro(a, b);
+    cout << "Resultado: " << resultado << endl;
+
+    return 0;
+}
